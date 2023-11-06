8da9d3be11642e94b5d065fa42c273ef1130710c
Arundas MK <codewitharun.com@gmail.com>
2023-11-06 23:32:54 +0530

    initial commit

---
+++ CoreDataToDo/CoreDataToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataToDo
+//
+//  Created by Arundas MK on 06/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
